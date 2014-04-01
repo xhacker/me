813f5787d96e825d4eff1b469547e1aef3a97ba5
Xhacker Liu <liu.dongyuan@gmail.com>
2014-04-01 16:14:16 -0700

    Resume.

--- hello.py
+++ hello.py
@@ -3,13 +3,21 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+
 @app.route('/cryptomeria')
 def cryptomeria():
     return render_template('cryptomeria.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
