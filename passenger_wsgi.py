a94f72ba134ab0e987398f2ad01ae8b415dbbb52
Xhacker Liu <liu.dongyuan@gmail.com>
2015-01-07 01:16:01 -0500

    Update virtualenv path.

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,5 +1,5 @@
 import sys, os
-INTERP = os.path.join(os.environ['HOME'], 'www', 'bin', 'python')
+INTERP = os.path.join(os.environ['HOME'], 'www', 'env', 'bin', 'python')
 if sys.executable != INTERP:
 	os.execl(INTERP, INTERP, *sys.argv)
 sys.path.append(os.getcwd())
